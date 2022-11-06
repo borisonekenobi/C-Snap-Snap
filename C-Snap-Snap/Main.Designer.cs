7d8b038b3753e4bebc4af827544a28eb2991109e
Boris <borisonekenobi@gmail.com>
2022-11-06 14:22:28 -0500

    Initial UI

---
+++ C-Snap-Snap/Main.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace C_Snap_Snap
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.language = new System.Windows.Forms.ComboBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.blockCloser = new System.Windows.Forms.Label();
+            this.files = new System.Windows.Forms.TabControl();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.language);
+            this.splitContainer1.Panel1MinSize = 45;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 45;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // language
+            // 
+            this.language.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.language.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.language.DropDownWidth = 120;
+            this.language.FormattingEnabled = true;
+            this.language.Items.AddRange(new object[] {
+            "Choose Language",
+            "C++"});
+            this.language.Location = new System.Drawing.Point(667, 12);
+            this.language.Name = "language";
+            this.language.Size = new System.Drawing.Size(120, 21);
+            this.language.TabIndex = 0;
+            this.language.SelectedIndexChanged += new System.EventHandler(this.language_SelectedIndexChanged);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer2.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.BackColor = System.Drawing.SystemColors.Control;
+            this.splitContainer2.Panel2.Controls.Add(this.blockCloser);
+            this.splitContainer2.Panel2.Controls.Add(this.files);
+            this.splitContainer2.Panel2.DoubleClick += new System.EventHandler(this.AddFile);
+            this.splitContainer2.Size = new System.Drawing.Size(800, 401);
+            this.splitContainer2.SplitterDistance = 169;
+            this.splitContainer2.TabIndex = 0;
+            this.splitContainer2.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitContainer2_SplitterMoved);
+            // 
+            // blockCloser
+            // 
+            this.blockCloser.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.blockCloser.AutoSize = true;
+            this.blockCloser.Location = new System.Drawing.Point(0, 388);
+            this.blockCloser.Name = "blockCloser";
+            this.blockCloser.Size = new System.Drawing.Size(13, 13);
+            this.blockCloser.TabIndex = 0;
+            this.blockCloser.Text = "<";
+            this.blockCloser.Click += new System.EventHandler(this.blockCloser_Click);
+            // 
+            // files
+            // 
+            this.files.Location = new System.Drawing.Point(0, 0);
+            this.files.Margin = new System.Windows.Forms.Padding(0);
+            this.files.Name = "files";
+            this.files.SelectedIndex = 0;
+            this.files.Size = new System.Drawing.Size(627, 401);
+            this.files.TabIndex = 1;
+            this.files.Visible = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            this.splitContainer2.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.Label blockCloser;
+        private System.Windows.Forms.ComboBox language;
+        private System.Windows.Forms.TabControl files;
+    }
+}
+
---
+++ C-Snap-Snap/Main.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_Snap_Snap
+{
+    public partial class Main : Form
+    {
+        private bool initDone = false;
+        private int prevIndex = 0;
+        private bool resettingLanguage = false;
+        private string path;
+        private Color primary = Color.FromArgb(141, 35, 15);
+        private Color secondary = Color.FromArgb(30, 67, 76);
+        private Color accent = Color.FromArgb(155, 79, 15);
+        private Color accentSecondary = Color.FromArgb(201, 158, 16);
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            splitContainer2.Panel2.BackColor = primary;
+            splitContainer2.Panel1.BackColor = secondary;
+            splitContainer1.Panel1.BackColor = secondary;
+            blockCloser.BackColor = secondary;
+            language.SelectedIndex = 0;
+
+            for (int i = 0; i < files.TabCount; i++)
+            {
+                files.SelectedIndex = i;
+                files.SelectedTab.BackColor = primary;
+            }
+
+            initDone = true;
+        }
+
+        private void splitContainer2_SplitterMoved(object sender, SplitterEventArgs e)
+        {
+            if (splitContainer2.SplitterDistance <= 50)
+            {
+                splitContainer2.Panel1Collapsed = true;
+                blockCloser.Text = ">";
+            }
+            else
+            {
+                splitContainer2.Panel1Collapsed = false;
+                blockCloser.Text = "<";
+            }
+        }
+
+        private void blockCloser_Click(object sender, EventArgs e)
+        {
+            if (splitContainer2.Panel1Collapsed)
+            {
+                splitContainer2.Panel1Collapsed = false;
+                blockCloser.Text = "<";
+            }
+            else
+            {
+                splitContainer2.Panel1Collapsed = true;
+                blockCloser.Text = ">";
+            }
+        }
+
+        private void language_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!initDone) return;
+            if (resettingLanguage)
+            {
+                resettingLanguage = false;
+                return;
+            }
+
+            if (files.Visible)
+            {
+                /*var saveChanges = MessageBox.Show("Would you like to save any changes?", "Warning!", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+                if (saveChanges == DialogResult.Cancel)
+                {
+                    resettingLanguage = true;
+                    language.SelectedIndex = prevIndex;
+                    return;
+                }*/
+            }
+
+            if (language.SelectedIndex == 0)
+            {
+                files.Visible = false;
+                prevIndex = 0;
+            }
+            else
+            {
+                files.Visible = true;
+                AddFile(sender, e);
+                prevIndex = language.SelectedIndex;
+            }
+        }
+
+        private void AddFile(object sender, EventArgs e)
+        {
+            if (!files.Visible) return;
+
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                RestoreDirectory = true,
+                //DefaultExt = FileExt(language.SelectedText),
+                Filter = FileExt(language.SelectedItem.ToString()) + "All files (*.*)|*.*"
+            };
+            //sfd.InitialDirectory = path;
+            var isFileSelected = sfd.ShowDialog();
+            if (isFileSelected == DialogResult.Cancel)
+            {
+                language.SelectedIndex = prevIndex;
+                return;
+            }
+
+            TabPage tabPage1 = new TabPage
+            {
+                Name = sfd.FileName,
+                Text = sfd.FileName,
+                BackColor = primary,
+                ForeColor = primary,
+                Font = new Font("Verdana", 12),
+                Width = 100,
+                Height = 100
+            };
+            files.TabPages.Add(tabPage1);
+        }
+
+        private string FileExt(string language)
+        {
+            switch (language)
+            {
+                case "C": return "C (*.c;*.i)|*.c;*.i|";
+                case "C#": return "C# (*.cs;*.csx;*.cake)|*.cs;*.csx;*.cake|";
+                case "C++": return "C++ (*.cpp;*.cc;*.cxx;*.c++;*.hpp;*.hh;*.h++;*.h;*.ii)|*.cpp;*.cc;*.cxx;*.c++;*.hpp;*.hh;*.h++;*.h;*.ii|";
+                case "Java": return "java";
+                case "JavaScript": return "js";
+                case "Python": return "py";
+                case "TypeScript": return "ts";
+                //TODO: add more languages
+            }
+            return language;
+        }
+    }
+}
