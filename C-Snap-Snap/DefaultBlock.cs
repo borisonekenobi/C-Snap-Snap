7eb92daf524e9607b94d70a44111404fe7af066f
Boris <borisonekenobi@gmail.com>
2022-11-09 21:15:52 -0500

    Created DefualtBlock class

---
+++ C-Snap-Snap/DefaultBlock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_Snap_Snap
+{
+    internal class DefaultBlock : Block
+    {
+        public bool Visible = true;
+
+        public DefaultBlock(string file, Block next, Block prev, Position pos) : base(file, next, prev, pos)
+        {
+            this.next = next;
+            this.prev = prev;
+            this.pos = pos;
+        }
+
+        override
+        public void Draw(PaintEventArgs e)
+        {
+
+        }
+    }
+}
