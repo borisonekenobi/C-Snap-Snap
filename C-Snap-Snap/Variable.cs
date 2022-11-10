ecf7714b5b1b3ca3ea54027b70aa902da5601948
Boris <borisonekenobi@gmail.com>
2022-11-09 21:16:45 -0500

    Added Variable class

---
+++ C-Snap-Snap/Variable.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_Snap_Snap
+{
+    internal class Variable : DefaultBlock
+    {
+        private readonly Size size = new Size(100, 30);
+        private readonly Color color = Color.Orange;
+
+        private string Type { get; set; }
+        private string Name { get; set; }
+        private string Value { get; set; }
+        private bool IsDefault { get; set; }
+
+        public Variable(string file, Block next, Block prev, Position pos, string type, string name, string value) : this(file, next, prev, pos, type, name, value, false)
+        {
+        }
+
+        public Variable(string file, Block next, Block prev, Position pos, string type, string name, string value, bool isDefault) : base(file, next, prev, pos)
+        {
+            this.next = next;
+            this.prev = prev;
+            this.pos = pos;
+            Type = type;
+            Name = name;
+            Value = value;
+            IsDefault = isDefault;
+        }
+
+        override
+        public void Draw(PaintEventArgs e)
+        {
+            if (Main.Files.SelectedTab.Name != file) return;
+            using (Pen pen = new Pen(color, 2))
+            {
+                e.Graphics.DrawRectangle(pen, new Rectangle(pos.X - 200, pos.Y - 100, size.Width, size.Height));
+            }
+        }
+    }
+}
