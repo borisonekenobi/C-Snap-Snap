3cf43fc33580eeb2dab753f1254bf321f6981ab7
Boris <borisonekenobi@gmail.com>
2022-11-09 21:14:54 -0500

    Created Position Class

---
+++ C-Snap-Snap/Position.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Snap_Snap
+{
+    internal class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Position(Point pos)
+        {
+            X = pos.X;
+            Y = pos.Y;
+        }
+    }
+}
