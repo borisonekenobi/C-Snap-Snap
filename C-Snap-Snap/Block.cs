3e23b3ba28d69ce56f9ad4c729c3cd4ccd0906bb
Boris <borisonekenobi@gmail.com>
2022-11-09 21:13:45 -0500

    Created Block class

---
+++ C-Snap-Snap/Block.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_Snap_Snap
+{
+    internal class Block
+    {
+        protected string file;
+        protected Block next;
+        protected Block prev;
+        protected Position pos;
+
+        public Block(string file, Block next, Block prev, Position pos)
+        {
+            this.file = file;
+            this.next = next;
+            this.prev = prev;
+            this.pos = pos;
+        }
+
+        virtual
+        public void Draw(PaintEventArgs e)
+        {
+        }
+    }
+}
